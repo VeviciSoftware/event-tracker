ee9ae9018d30711cc7898f30cd9382ac1e722c5b
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-09-11 16:07:53 -0300

    :sparkles: feat: usando api fake para mostrar eventos por padrão

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import style from './App.module.scss';
-import Card from './components/Card';
-import Formulario from './components/Formulario';
-import Calendario from './components/Calendario';
-import ListaDeEventos from './components/ListaDeEventos';
-import { RecoilRoot } from 'recoil'
+import style from "./App.module.scss";
+import Card from "./components/Card";
+import Formulario from "./components/Formulario";
+import Calendario from "./components/Calendario";
+import ListaDeEventos from "./components/ListaDeEventos";
+import { RecoilRoot } from "recoil";
+import { Suspense } from "react";
 
 function App() {
   return (
     <RecoilRoot>
-      <div className={style.App}>
-        <div className={style.Coluna}>
-          <Card>
-            <Formulario />
-          </Card>
-          <hr />
-          <Card>
-            <ListaDeEventos />
-          </Card>
+      <Suspense fallback={<div>Carregando...</div>}>
+        <div className={style.App}>
+          <div className={style.Coluna}>
+            <Card>
+              <Formulario />
+            </Card>
+            <hr />
+            <Card>
+              <ListaDeEventos />
+            </Card>
+          </div>
+          <div className={style.Coluna}>
+            <Calendario />
+          </div>
         </div>
-        <div className={style.Coluna}>
-          <Calendario />
-        </div>
-      </div>
+      </Suspense>
     </RecoilRoot>
   );
 }
--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -1,5 +1,6 @@
 import { selector } from "recoil";
 import { filtroDeEventos, listaDeEventosState } from "../atom";
+import { IEvento } from "../../interfaces/IEvento";
 
 export const eventosFiltradosState = selector({
     key: 'eventosFiltradosState',
@@ -15,4 +16,17 @@ export const eventosFiltradosState = selector({
       })
       return eventos
     }
-  })
+})
+
+export const eventosAsync = selector({
+    key: 'eventosAsync',
+    get: async () => {
+      const response = await fetch('http://localhost:8080/eventos')
+      const eventos: IEvento[] = await response.json()
+      return eventos.map(evento => ({
+        ...evento,
+        inicio: new Date(evento.inicio),
+        fim: new Date(evento.fim)
+      }))
+    }
+})
