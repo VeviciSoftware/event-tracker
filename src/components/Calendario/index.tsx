02dfa9c31f4810955451ba6c1930f9af436c75dc
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-09-11 14:57:36 -0300

    :sparkles: feat: filtro aplicado também para o calendário

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -2,9 +2,10 @@
 import React from 'react'
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarView } from 'kalend'
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
 import useListaDeEventos from '../../state/hooks/useListaDeEventos';
+import useAtualizarEvento from '../../state/hooks/aoAtualizarEvento';
 
 interface IKalendEvento {
   id?: number
@@ -18,6 +19,7 @@ const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
   const eventos = useListaDeEventos()
+  const atualizarEvento = useAtualizarEvento()
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10)
@@ -32,6 +34,23 @@ const Calendario: React.FC = () => {
       color: 'blue'
     })
   })
+
+  const onEventDragFinish: OnEventDragFinish = (
+    kalendEventoInalterado: CalendarEvent,
+    kalendEventoAtualizado: CalendarEvent
+  ) => {
+    const evento = eventos.find(evento => evento.descricao === kalendEventoAtualizado.summary)
+    if (evento) {
+      const eventoAtualizado = {
+        ...evento
+      }
+      eventoAtualizado.inicio = new Date(kalendEventoAtualizado.startAt)
+      eventoAtualizado.fim = new Date(kalendEventoAtualizado.endAt)
+      atualizarEvento(eventoAtualizado)
+    }
+
+  };
+
   return (
     <div className={style.Container}>
       <Kalend
@@ -44,6 +63,7 @@ const Calendario: React.FC = () => {
         calendarIDsHidden={['work']}
         language={'customLanguage'}
         customLanguage={ptBR}
+        onEventDragFinish={onEventDragFinish}
       />
     </div>
   );
